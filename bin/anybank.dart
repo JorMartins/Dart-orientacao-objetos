5ccb32c681cf27f204d5aa61ad1617b997dcbe4c
Jorge Martins <martins.jorge0@gmail.com>
2025-10-08 14:26:20 -0300

    v2

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -15,12 +15,43 @@ void main() {
 
   contaRoberta.saldo = 5000;
 
-  print(contas[1].saldo);
+  print("");
+  print("");
+
+  for (Conta conta in contas){
+    print("TITULAR: ${conta.titular}   /  SALDO: ${conta.saldo}");
+  }
+
+  contaMatheus.receber(500);
+
+  print("");
+
+  for (Conta conta in contas){
+    print("TITULAR: ${conta.titular}   /  SALDO: ${conta.saldo}");
+  }
+
+  contaMatheus.enviar(200);
+
+  print("");
+
+  for (Conta conta in contas){
+    print("TITULAR: ${conta.titular}   /  SALDO: ${conta.saldo}");
+  }
 }
+
+
 
 class Conta {
   String titular;
   double saldo;
 
   Conta(this.titular, this.saldo);
+  
+  void receber(double valor) {
+    saldo += valor;
+  }
+  
+  void enviar(double valor) {
+    saldo -= valor;
+  }
 }
