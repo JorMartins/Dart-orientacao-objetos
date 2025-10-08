3197fddeecc27530ac2f54b1db020d8730a5e765
Jorge Martins <martins.jorge0@gmail.com>
2025-10-08 13:58:35 -0300

    v1

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,5 +1,26 @@
-import 'package:anybank/anybank.dart' as anybank;
 
-void main(List<String> arguments) {
-  print('Hello world: ${anybank.calculate()}!');
+void main() {
+  Conta contaMatheus = Conta("Matheus", 1000);
+  Conta contaRoberta = Conta("Roberta", 2000);
+
+  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
+
+  print(contaMatheus.titular);
+  print(contaMatheus.saldo);
+  print("");
+
+  print(contaRoberta.titular);
+  print(contaRoberta.saldo);
+  print("");
+
+  contaRoberta.saldo = 5000;
+
+  print(contas[1].saldo);
+}
+
+class Conta {
+  String titular;
+  double saldo;
+
+  Conta(this.titular, this.saldo);
 }
